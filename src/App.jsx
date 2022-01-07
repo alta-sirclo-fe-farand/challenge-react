ebfbee1a596de74c1c91137edf40eb779db751a5
fermidrates <albertfarand@gmail.com>
2022-01-08 00:02:28 +0700

    js to jsx extension

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import React from 'react';
+import TopMenu from './topMenu.jsx';
+import BottomMenu from './bottomMenu';
+import Home from './home';
+// import About from './about';
+// import Contact from './contact';
+
+
+function App() {
+  return null;
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Home from './home';
+import About from './about'
+import Contact from './contact';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <About />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
